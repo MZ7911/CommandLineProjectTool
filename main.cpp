e819ebd9bc1c460b6bc7cde57d07446c2f402885
Miller Piao <76028025+gzmiller@users.noreply.github.com>
2021-03-01 20:34:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 /// This is a functional implementation.
 
+/// Types
+
 typedef int InputDataType;
 typedef const InputDataType ConstIDT;
 typedef InputDataType & EditableIDT;
@@ -12,11 +14,31 @@ typedef ConstODT & FastODT;
 
 typedef void StatementType;
 
+/// Prototypes
+
 FastIDT input();
 FastODT calculate(FastIDT inputData);
 StatementType output(FastODT outputData);
 
+/// Main
+
 int main() {
     output(calculate(input()));
     return 0;
+}
+
+/// Implementations
+
+FastIDT input() {
+    InputDataType res;
+    return res;
+}
+
+FastODT calculate(FastIDT inputData) {
+    OutputDataType res;
+    return res;
+}
+
+StatementType output(FastODT outputData) {
+
 }
